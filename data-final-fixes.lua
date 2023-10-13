c967c8b3310dc487a293fd73eae34f7abcbcf173
Alienmenno <2185834+Alienmenno@users.noreply.github.com>
2023-10-13 21:52:23 +0200

    Fix burnt fuel settings for boats

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,6 +2,9 @@ require("defines")
 
 if mods["IndustrialRevolution3"] and mods["cargo-ships"] then
     data.raw.locomotive["cargo_ship_engine"].burner.fuel_categories = {"steam-cell","canister","barrel","battery"}
+	data.raw.locomotive["cargo_ship_engine"].burner.burnt_inventory_size = data.raw.locomotive["cargo_ship_engine"].burner.fuel_inventory_size
     data.raw.locomotive["boat_engine"].burner.fuel_categories = {"steam-cell","canister","barrel","battery"}
+	data.raw.locomotive["boat_engine"].burner.burnt_inventory_size = data.raw.locomotive["boat_engine"].burner.fuel_inventory_size
     data.raw.car["indep-boat"].burner.fuel_categories = {"steam-cell","canister","barrel","battery"}
+	data.raw.car["indep-boat"].burner.burnt_inventory_size = data.raw.car["indep-boat"].burner.fuel_inventory_size
 end
