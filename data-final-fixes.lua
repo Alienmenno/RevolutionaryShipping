db5fb84cc4e9df24a05a13d288cde85a83945b66
Alienmenno <2185834+Alienmenno@users.noreply.github.com>
2023-10-13 21:59:07 +0200

    Update data-final-fixes.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,10 +1,22 @@
 require("defines")
 
 if mods["IndustrialRevolution3"] and mods["cargo-ships"] then
-    data.raw.locomotive["cargo_ship_engine"].burner.fuel_categories = {"steam-cell","canister","barrel","battery"}
-	data.raw.locomotive["cargo_ship_engine"].burner.burnt_inventory_size = data.raw.locomotive["cargo_ship_engine"].burner.fuel_inventory_size
-    data.raw.locomotive["boat_engine"].burner.fuel_categories = {"steam-cell","canister","barrel","battery"}
-	data.raw.locomotive["boat_engine"].burner.burnt_inventory_size = data.raw.locomotive["boat_engine"].burner.fuel_inventory_size
-    data.raw.car["indep-boat"].burner.fuel_categories = {"steam-cell","canister","barrel","battery"}
-	data.raw.car["indep-boat"].burner.burnt_inventory_size = data.raw.car["indep-boat"].burner.fuel_inventory_size
+
+    local default_vehicle_fuel_categories = {"steam-cell","canister","barrel","battery"}
+    
+    local vehicles = {
+        locomotive = "cargo_ship_engine",
+        locomotive = "boat_engine",
+        car = "indep-boat",
+    }
+    
+    for name,vehicle in pairs(vehicles) do
+        local prototype = data.raw[vehicle][name]
+        if prototype and prototype.burner then
+            prototype.burner.fuel_category = nil
+            prototype.burner.fuel_categories = default_vehicle_fuel_categories
+            prototype.burner.burnt_inventory_size = prototype.burner.fuel_inventory_size
+        end
+    end
+
 end
